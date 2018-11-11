7ea8a71bbc5d38e687099bfa83fc62731379bccf
Anastasiya <ipshapovalova@gmail.com>
2018-11-11 23:52:11 +0700

    dev

--- src/js/PageProductCard.js
+++ src/js/PageProductCard.js
@@ -1,5 +1,17 @@
 const PageProductCard = {
   init: function() {
     console.log('PageProductCard.init')
-  }
+    this.initSliderVideo()
+  },
+
+  initSliderVideo: function(){
+    $(document).ready(function(){
+      $(".js-slider-video").owlCarousel({
+        nav: false,
+        dots: true,
+        dotsContainer: '.js-slider-video-dots',
+        items: 1,
+      })
+    });
+  },
 }
