4bb02fa428cd7034282a2350091eff52cc92f1b3
Anastasiya <ipshapovalova@gmail.com>
2018-11-13 14:54:44 +0700

    dev

--- src/js/PagePartners.js
+++ src/js/PagePartners.js
@@ -1,5 +1,17 @@
 const PagePartners = {
   init: function() {
     console.log('PagePartners.init')
-  }
+    this.initBanner()
+  },
+
+  initBanner: function(){
+    $(document).ready(function(){
+      $(".js-banner").owlCarousel({
+        nav: false,
+        dots: true,
+        dotsContainer: '.js-banner-dots',
+        items: 1,
+      })
+    });
+  },
 }
