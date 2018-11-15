45b0435861e9cde933d98d074c0dfe3e06f6756e
Anastasiya <ipshapovalova@gmail.com>
2018-11-15 18:41:34 +0700

    dev

--- src/js/PageCatalog.js
+++ src/js/PageCatalog.js
@@ -1,5 +1,19 @@
 const PageCatalog = {
   init: function() {
     console.log('PageCatalog.init')
-  }
+    this.initSliderRange()
+  },
+
+  initSliderRange: function(){
+    noUiSlider.create(document.getElementById('slider'), {
+      start: [20, 80],
+      connect: true,
+      range: {
+        'min': 0,
+        'max': 100
+      }
+    });
+  },
+
 }
+
