eb9bd8549697cf7d6b54d0148369ba5c5d59ad25
maximkasatkin <cmIMav0mv0>
2018-11-18 12:44:56 +0700

    dev

--- src/js/PageService.js
+++ src/js/PageService.js
@@ -1,5 +1,24 @@
 const PageService = {
   init: function() {
     console.log('PageService.init')
+    this.initInstructionHeights()
+  },
+
+  initInstructionHeights() {
+    const $el = $('.js-instructions')
+    const heights = []
+    const $uls = $el.find('.instruction__ul')
+    const $ulsLength = $uls.length
+
+    let maxRowLength = 0
+    $uls.forEach(function($ul){
+      const length = $ul.find('li').length
+      if (length > maxRowLength) maxRowLength = length
+    })
+
+    for(let i = 0; i < $ulsLength; i++) {
+      const rowHeights = []
+    }
+    for(let i = 0; i < maxRowLength; i++) {}
   }
 }
