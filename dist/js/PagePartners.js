dd448b050cc6a8f6a300cacb5f50a004cabe3821
Anastasiya <ipshapovalova@gmail.com>
2018-11-21 17:35:16 +0700

    dev

--- dist/js/PagePartners.js
+++ dist/js/PagePartners.js
@@ -1,18 +1,27 @@
 const PagePartners = {
-  init: function() {
-    console.log('PagePartners.init')
-    this.initBanner()
-    RedsMenu.init()
-  },
+    init: function () {
+        console.log('PagePartners.init')
+        this.initBanner()
+        RedsMenu.init()
+        this.handleAccordionsDocs()
 
-  initBanner: function(){
-    $(document).ready(function(){
-      $(".js-banner").owlCarousel({
-        nav: false,
-        dots: true,
-        dotsContainer: '.js-banner-dots',
-        items: 1,
-      })
-    });
-  },
+    },
+
+    initBanner: function () {
+        $(document).ready(function () {
+            $(".js-banner").owlCarousel({
+                nav: false,
+                dots: true,
+                dotsContainer: '.js-banner-dots',
+                items: 1,
+            })
+        });
+    },
+
+    handleAccordionsDocs: function () {
+        $('.js-docs-accordion').on('click', function(){
+            $(this).toggleClass('m-open')
+        })
+            },
+
 }
